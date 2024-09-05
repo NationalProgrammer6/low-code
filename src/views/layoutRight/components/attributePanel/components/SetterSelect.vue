2625ec3f16a277f1bd136667bd0feda743ea53af
shiqiaoquan <863569563@qq.com>
2024-09-05 07:04:02 +0000

    添加组件

---
+++ src/views/layoutRight/components/attributePanel/components/SetterSelect.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineEmits } from 'vue';  
+import { Select } from 'ant-design-vue';
+const props = defineProps({
+    type: {
+        type: String,
+    },
+    initialValue: {
+        type: any
+    },
+    options: {
+        type: Array,
+        default: ()=>{
+            return []
+        }
+    }
+})
+const value = ref("")
+const emits = defineEmits(['change'])
+function change(value){
+    emits('change',value)
+}
+</script>
+
+<template>
+    <Select @change="change" :options="options" v-model:value="value"></Textarea>
+</template>
